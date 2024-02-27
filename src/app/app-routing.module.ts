a54df1cafb4309be388f257c0ba610f139a7a92e
Mitul <mitulgondaliya27@gmail.com>
2024-02-27 18:34:07 +0530

    send data from parent to child

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildComponent } from './components/child/child.component';
+import { ParentComponent } from './components/parent/parent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'child', component: ChildComponent},
+  {path: 'parent', component: ParentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
