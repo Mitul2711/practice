e2db0e1fdfe6390a0fe2f08b98fc1201c96e9538
Mitul <mitulgondaliya27@gmail.com>
2024-02-28 10:47:17 +0530

    send data from child to parent

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,12 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent {
+export class ChildComponent implements OnInit {
+  
+  @Input() data: any[];
+  
+  
+  @Output() parentFunction: EventEmitter<any> = new EventEmitter();
+  
+  ngOnInit(): void {
+    this.parentFunction.emit(this.tableData);
+  }
 
-  @Input() tableData: any[];
-
+  tableData: any[] = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  ]
 }
